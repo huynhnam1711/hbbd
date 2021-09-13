190b4aaf28373d9097e62fa5c0f9067d8da49cbd
huynhnam1711 <89021166+huynhnam1711@users.noreply.github.com>
2021-09-13 07:22:21 +0700

    Add files via upload

---
+++ namhuynh.cpp
@@ -0,0 +1,164 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<iostream>
+/*#include<string.h>
+void nhap(int &n)
+{
+	do{
+		printf("\nMoi Nhap So Ban Yeu Thich:");
+		scanf("%d", &n);
+		if(n!= 17)
+			printf("\nBan Nhap Sai, Hay Nhap Lai .... ");
+	}while(n!= 17);*/
+
+int main()
+
+	{
+	char n[11];
+	char a[11]="17/11/2001";
+	do{
+		printf("\nMoi Nhap Ngày Sinh Cua Ban:");
+		scanf("%s", &n);
+		if(strcmp(a,n)!=NULL)
+			printf("\nBan Nhap Sai, Hay Nhap Lai :(( ");
+	}while(strcmp(a,n)!=NULL);
+	
+	 std::cout << "Da Xu Ly: 10%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 20%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 30%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 40%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 50%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 60%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 70%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 80%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 90%" << std::endl;
+    sleep(1);
+     std::cout << "Da Xu Ly: 100%" << std::endl;
+    sleep(2);
+/*	printf("Da Xu Ly: 10%%");
+ 	printf("\nDa Xu Ly: 20%%");
+ 	printf("\nDa Xu Ly: 30%%");
+ 	printf("\nDa Xu Ly: 40%%");
+ 	printf("\nDa Xu Ly: 50%%");
+ 	printf("\nDa Xu Ly: 60%%");
+ 	printf("\nDa Xu Ly: 60%%");
+ 	printf("\nDa Xu Ly: 80%%");
+ 	printf("\nDa Xu Ly: 90%%");
+ 	printf("\nDa Xu Ly: 100%%");
+ 	printf("\n");*/
+ 	/*printf("\nHoan Tat ..... ");*/
+ 	std::cout << "\nHoan Tat ....." << std::endl;
+    sleep(2);
+ 	
+ 	std::cout << "\nyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<< std::endl;
+ 	sleep(1);
+	std::cout <<"yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<< std::endl;
+	sleep(1);
+	std::cout <<"yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<< std::endl;
+	sleep(1);
+	std::cout <<"yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<< std::endl;
+	sleep(1);
+	std::cout <<"yyyyyyssssyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<< std::endl;
+	sleep(1);
+	std::cout <<"yyyyysssssssssssssssssssyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssssysssyyyyyhhhhhhyssyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyysssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssssssyhhhdmNNMMMMNNhyssssssysssssssssssssssssssssssssssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssssydmmNNNNMMMMMMMMMmssssssssssssssssssssssssssssssssssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssshNNNNMMMNNmdhhdNMMNysssssssssssssssssssssssssssssssssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssymNNNmhso+/:--/oymNMmsssssssssssssssssssssssssssssssssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssyNMds/-.....-:+ooydNMhssssssssssssssssssssssssssssssssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"sssssssssssssssssssssssssssssssssssdd+:---.----://ohmmNmssssssssssssssssssssssssssssssssssssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"sssssssssssssssssssssssssssssssssssy+----:////oyhhhdmmmNsssssssssssssssssssssssssssssssssoosoooooooo"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssssssssssssssssssssssssss/::+yhhyyyddmmmmmmmNyyoooooooosssssssoosoooooooooooooooooooooooo"<< std::endl;
+	sleep(1);
+	std::cout <<"sssssssssssosssssssssssssssssssssssso-/shdmds/shddhdddmmmmoooooooooooooooooooooooooooooooooooooooooo"<< std::endl;
+	sleep(1);
+	std::cout <<"ssoooooooooooooooooosssssoooooooooo+/:--://:.`.::::+ydddmhooooooooooooooooooooooooooooooooooooooo+oo"<< std::endl;
+	sleep(1);
+	std::cout <<"ooooooooooooooooooooooooooooooooooo+o:---.`  `:/--:+oyyddo++++++++++ooooooooo+++++++++++++++++++++++"<< std::endl;
+	sleep(1);
+	std::cout <<"ooooooooooooooooooooooooooooooooooo+:---.```..:+//++syhyo++++++++++++++++++++++++++++++++///////////"<< std::endl;
+	sleep(1);
+	std::cout <<"ooooooooooooooooooooooooooooooo+o++++:---...-:-++++/shs+++//++++++////++++/++++/////////////////////"<< std::endl;
+	sleep(1);
+	std::cout <<"++++++++++++++++++++++++++++++++++++++///:::::-/+oooyds/////////////////////////////////:::::::::://"<< std::endl;
+	sleep(1);
+	std::cout <<"++++++++++++//++++/+++++++++++++////////////::-/osyhmMd+hdho//////::::::::::::::::::::::::::::::::::"<< std::endl;
+	sleep(1);
+	std::cout <<"//////////////////////////////////////osyhhhyysshdNMMMMNNNNNdho+/:::::::::::::::::::::::::::::::::::"<< std::endl;
+	sleep(1);
+	std::cout <<"//////////////////////////////////////shdmNNNNNNNNNMNNNNNNNNNdhyyyso+/::::::::::::::::::::::::::::::"<< std::endl;
+	sleep(1);
+	std::cout <<":/:::::::::::::::::::::::::::::::::/++shmmdNMMNNNNNNNNNNNNdddmmmmmmmmmmho/::::::::::::::::::::::::::"<< std::endl;
+	sleep(1);
+	std::cout <<":::::::::::::::::::::::::::::::/oyhddyyyyhddMNNmmNNNNNNMMMMNNmmmmmmmNmNNmmy//////////:::::::::::::::"<< std::endl;
+	sleep(1);
+	std::cout <<":::::--:---:::::::::::::::::::ohddddddddddmmNNMMMMMMMMMMMNmmmmmmmmmNMmNNmmmmhoo+++++++++////////////"<< std::endl;
+	sleep(1);
+	std::cout <<":::::::::::::::::::///::::://+dddddddddmmmmNNNNNNNNNNMMMNmmmmmNNNmmNMmNmNNNNNdoooooo++++++++++++++++"<< std::endl;
+	sleep(1);
+	std::cout <<":::::::::::::::::::////::::/odmdddddddddmmmmmmmmmmmmmmmmmmmmmNNNNmNMNNmNMMNNNNmo+oooooooo++++o++++++"<< std::endl;
+	sleep(1);
+	std::cout <<":::::::::::::::::://///+++oosmmmmoddhyoyyhmmmmmmmmmmmmh+//smNNNNNmNMNNNNMMNNNNNdsyyyysssssssssoooooo"<< std::endl;
+	sleep(1);
+	std::cout <<"///:////+++oooooossssssssssyhmmmhsdoooyddo+soys+ossyodyso/mNNNNNNNMMNMMMMMNNNNNNmhhhhhhhhhhhhhhhhhyy"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssyyyyyyyyyyyyyyyssssssosshNmd+sdssydddhhhy:ohyysmhs/+osNNNNNNNNMMMMNMMMNNNNNNNmhhhhhhhhhhhhhhhhhh"<< std::endl;
+	sleep(1);
+	std::cout <<"ooyyyyyyysyhhyyyyysosssyssoohNmyyddmdo/sddomdyh+/+dmmyssydNNMMNNNMMMMMNMMMNNmNNNNNmddddhdhhhhhhhhhhh"<< std::endl;
+	sleep(1);
+	std::cout <<"ssyyyyyyyysoosyso+//+osss/++hNNhys+oo/sdy+/hms:o+ooo/+syymNMMNNNNMMMMMNMMMNmmNNNNMNhhhhhhhhdhhhhhhhh"<< std::endl;
+	sleep(1);
+	std::cout <<"ssyssosooo++osyso+/++ooo+/+ohNN+oyhhyhmmhmmmmyymdhhdysssyNMMMNNNMMMMMMNMMMNmmNNNNNMmyhhhhhhhhhhhhhhh"<< std::endl;
+	sleep(1);
+	std::cout <<"ooso+/+///+oooooooooo+oosoo+yNmsmmmmmmmmNNNmmmNNNNNNhhhsdNMMNNNMMMMMMMNMMNNmmmNNmmmMdyyyyyyyhhhhhhhh"<< std::endl;
+	sleep(1);
+	std::cout <<"soooooooosoo+oooossssoosysosdNdhmmmmmNmNNNNmmNNdmNNmhhhoNMMMNNNMMMMMMMMMNmmmmNm:+/oymddhhyooyyyyhhhy"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssssssssoooooosssssosssss/dmNmmNNNmNNNmmNNm:osmmyyydMMMNNNMMMMMMMMNNmmhNNMN:yo+odmhhyysyhhhhhhhh"<< std::endl;
+	sleep(1);
+	std::cout <<"ssysyyyyyyssssoooooooosoosssy+NmNmmNNmNNNmmmNNd/-/mhhdomMMNNNMMMMMMMMMMNNNhmmMNohyo+oddhhhhhhhhddddd"<< std::endl;
+	sleep(1);
+	std::cout <<"ssssssyyyssssssssssosossssyysymNNmmNNNNNmmmNNNy++yNmdddMMMNNNMMMMMMMMMMMMNNNNNMmmmmddmmhhhyyyyyyhhdm"<< std::endl;
+	sleep(1);
+	std::cout <<"sssssossssssyssssyssoooossyyyymNmmmmmmNmmmmNNNdmmNNNNNMMMNNNMMMMMMMMMMMMMMNNNNNNNNNNMMMhhysyysso++//"<< std::endl;
+	sleep(1);
+	std::cout <<"oooooooooooosssossssooossssssymNmmmmmmNmmmNNNNNNNNNNNNMMNNNNMMMMMMMMMMMMmmmmmmNNNNNNMMMNssssso+//:::"<< std::endl;
+	sleep(1);
+	std::cout <<"yssssssssyssssossyyyssssssossymNmmmNmmmmmNNNNNNNNNNNNMMMNNNNMMMMMMMMMMNdyhhhddddmmdmNMMMhsssssssssso"<< std::endl;
+	sleep(1);
+	std::cout <<"ddddhhhhhyyyyyyyhhyyyhhyhyyyyhmNmmmNmmmmmNNNNNNNNNNNNMMNMNNMMMMMMMNmddhyyyyyyyhddmddmmmNdsssssyysyyy"<< std::endl;
+	sleep(1);
+	std::cout <<"NNNNNNNNNNNNNmmdddddddhhhydmNNNNmmmNmmmmNNNNNNNNNNNNMMNMNNNNNNmmmhdhyyyyyyyyhhhhdmNNNNNNysssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"mNNNNNNhhdddmmmmmmdmmmmmhmmmmNNmmmmmmmmNNNNNNNNNNNNNMMMMNNmdyyyhhydhyhhyhhhhhddmmNNNNMMNysssssssssss"<< std::endl;
+	sleep(1);
+	std::cout <<"dNMMMMMs/+oooosysosyyyyhmy/+sdmmmmmmmmmNNNmmNNNNmmNMMMNNNNNNmyhyyhyhyyhhhhhdddmmNNNNMMMmdddhhhhhyyyy"<< std::endl;
+	sleep(1);
+	std::cout <<"mNMMMMMs//++++ooos+++++sdyddddmmmmmmmmNNNNmNNmmmmmdNNNNNNNNNNhdhyyyhddddhdddddNNNNMMNdddhhhhhhhhdddd"<< std::endl;
+	sleep(1);
+	std::cout <<"/dMMMMMs:/++/+++++///++ymdddmmmmmmmmmNNNmmmmmmmmmm:sNNNNNNMMMhdmyyhdNNNmmmmNmmNNMMMMMNNNmmmNNmmddddd"<< std::endl;
+	sleep(1);
+	std::cout <<":yMMMMMs-::///:::/+++++dmmmdmmmmmmmmmmmmmmmmmmmmmmy-hNNNMMMMMhmNmhhdNNmNNmmmmNMMMMMMMmNNmmNNNNNNmNmN"<< std::endl;
+	sleep(1);
+	std::cout <<":yMMMMMy-:::::::::/+///hmmmmmmmmmmmmmmmmmmmmmmmmNNNo/mMMMMMMNhmmNdhdNNNNNNmdssydhyhhhyhdddmmmmNNmNmm"<< std::endl;
+	sleep(1);
+    return 0;
+}
